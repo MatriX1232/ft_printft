2de61724cb5b548145ab54f0ac372ab08b588011
MatriX1232 <msolinsk@LAPTOP-1DVV0C81.localdomain>
2024-03-17 17:17:39 +0100

    First commit | Git innit | Added new files

---
+++ ftprintf.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ftprintf.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 17:01:59 by msolinsk          #+#    #+#             */
+/*   Updated: 2024/03/17 17:14:57 by msolinsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ftprintf.h"
+
+int	ft_get_arg_count(char *str)
+{
+	int	count;
+
+	count = 0;
+
+}
+
+
+int	ft_printf(char *str)
+{
+	int	counter;
+
+	counter = 0;
+
+}
---
+++ ftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ftprintf.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 16:59:28 by msolinsk          #+#    #+#             */
+/*   Updated: 2024/03/17 17:06:29 by msolinsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FTPRINTF
+# define FTPRINTF
+
+#include <unistd.h>
+#include <stdarg.h>
+
+int	ft_printf(char *str, ...);
+
+#endif
